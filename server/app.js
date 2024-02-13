37fe7b0dc7e2de39331a02a96bc7bff00bcf4a70
Margery <Margeryolethea@gmail.com>
2024-02-13 09:17:41 +0700

    setup server side

---
+++ server/app.js
@@ -0,0 +1,22 @@
+if (process.env.NODE_ENV !== "production") {
+  require(`dotenv`).config();
+}
+
+const express = require("express");
+const cors = require(`cors`);
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER DI SINI
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
