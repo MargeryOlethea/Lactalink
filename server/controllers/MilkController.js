2f678278efe489c72ace44eeb8193588cb058db0
unknown <reinardusk@gmail.com>
2024-02-15 15:32:33 +0700

    adding mongodb_string

--- server/controllers/MilkController.js
+++ server/controllers/MilkController.js
@@ -72,24 +72,66 @@ TOTAL: 	32 poin,
 
       // console.log(milks)
 
-      console.log(userLoginDetail)
+      // console.log(userLoginDetail)
 
-      // const milksWithCompability = milks.map(milk => {
-      //   let score = 0
-      //   if (milk.userdetail.gender == userLoginDetail.gender) {
-      //     score += 2
-      //   }
+      const milksWithCompability = milks.map(milk => {
+        let score = 0
+        if (milk.userdetail.gender == userLoginDetail.gender) {
+          score += 2
+        }
+        if (milk.userdetail.bloodRhesus == userLoginDetail.bloodRhesus) {
+          score += 5
+        }
+        if (milk.userdetail.bloodType == userLoginDetail.bloodType) {
+          score += 2
+        }
+        if (milk.userdetail.halal == userLoginDetail.halal) {
+          score += 5
+        }
+        if (milk.userdetail.egg == userLoginDetail.egg) {
+          score += 2
+        }
+        if (milk.userdetail.dairy == userLoginDetail.dairy) {
+          score += 2
+        }
+        if (milk.userdetail.nuts == userLoginDetail.nuts) {
+          score += 2
+        }
+        if (milk.userdetail.soy == userLoginDetail.soy) {
+          score += 2
+        }
+        if (milk.userdetail.seafood == userLoginDetail.seafood) {
+          score += 2
+        }
+        if (milk.userdetail.flourOrWheat == userLoginDetail.flourOrWheat) {
+          score += 2
+        }
+        if (milk.userdetail.readMeat == userLoginDetail.readMeat) {
+          score += 2
+        }
+        if (milk.userdetail.spicyFood == userLoginDetail.spicyFood) {
+          score += 2
+        }
+        if (milk.userdetail.caffeine == userLoginDetail.caffeine) {
+          score += 2
+        }
 
-      // })
+
+        return { ...milk, score }
+      })
+
+      // console.log(milksWithCompability)
 
       res.status(200).json({
         message: "Successfully get milks",
-        data: milks
+        data: milksWithCompability
       })
     } catch (error) {
       next(error)
     }
   }
+
+  static async
 }
 
 module.exports = MilkController
