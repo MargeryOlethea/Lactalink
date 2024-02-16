572247ad66f335ce5ab26d49b3ce4a0a7abd4ba5
Margery <Margeryolethea@gmail.com>
2024-02-16 17:17:53 +0700

    adjusting route

--- server/routers/chats.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const { createChat, getChats } = require("../controllers/ChatController")
-const router = express.Router()
-
-router.post("/:roomId", createChat)
-router.get("/:roomId", getChats)
-
-module.exports = router
