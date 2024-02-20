51a6b51d9dfa3b2a8abf5503b02a667991ca54f5
Margery Olethea <148076723+MargeryOlethea@users.noreply.github.com>
2024-02-20 17:33:00 +0700

    Delete client directory on server branch

--- client/helpers/sliceLocation.ts
+++
@@ -1,9 +0,0 @@
-export default function sliceStringToPairs(text: string) {
-  if (text.length % 2 !== 0) {
-    throw new Error("String length must be divisible by 2");
-  }
-
-  return Array.from({ length: text.length / 2 }, (_, i) =>
-    text.slice(i * 2, i * 2 + 2),
-  );
-}
